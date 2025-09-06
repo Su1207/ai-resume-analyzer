7c1d389ee6fe56efd329fc28ed45a71564a768ff
suraj maheshwari <surajmaheshwari159@gmail.com>
2025-09-07 01:46:19 +0530

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +14,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    // this will redirect user to the desired page which he wanted to access before signing in
+    useEffect(()=>{
+        if(!auth.isAuthenticated){
+            navigate('/auth?next=/');
+        }
+    },[auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+      <section className="main-section">
+          <div className="page-heading py-16">
+              <h1>Track Your Applications & Resume Ratings</h1>
+              <h2>Review your submission and check AI_powered feedback.</h2>
+          </div>
+
+          {resumes.length > 0 && (
+                  <div className="resumes-section">
+                      {resumes.map((resume)=>(
+                          <ResumeCard key={resume.id} resume={resume}/>
+                      ))}
+                  </div>
+              )}
+      </section>
+  </main>;
 }
